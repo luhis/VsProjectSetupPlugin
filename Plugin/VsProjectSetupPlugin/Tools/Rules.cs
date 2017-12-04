7e2cacacc1f5e070d8a3ba2b21206a33bf5d8051
luhis <mccorry@gmail.com>
2017-12-04 17:10:50 +0000

    Update Rules.cs

--- Plugin/VsProjectSetupPlugin/Tools/Rules.cs
+++ Plugin/VsProjectSetupPlugin/Tools/Rules.cs
@@ -13,7 +13,7 @@
                     new Rule("Projects without StyleCop.MsBuild installed", IsNotDbAndDoesNotHaveStyleCop),
                     new Rule("Projects with StyleCop Treat Errors As Warnings not set to false", IsNotDbAndDoesNotHaveStyleCopSetting),
                     new Rule("Projects that are not endpoints with app.config files", ProjectClassificationTools.HasAppDotConfigButNotEndPoint),
-                    new Rule("Projects with wrong version setup", VersionTools.HasIncorrectVersion)
+                    new Rule("Projects with wrong .Net version setup", VersionTools.HasIncorrectVersion)
                 };
 
         private static bool IsNotDbAndDoesNotHaveStyleCop(Proj p) => !ProjectClassificationTools.IsDatabaseProject(p) && !NuGetTools.HasStyleCopInstalled(p);
