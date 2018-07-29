5854e337235073148e387381f2620e26856fb767
Matthew McCorry <mccorry@gmail.com>
2018-07-29 20:44:19 +0100

    Refactoring

---
+++ Plugin/VsProjectSetupPlugin/Models/Info.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using VsProjectSetupPlugin.Tools;
+
+namespace VsProjectSetupPlugin.Models
+{
+    public class Info
+    {
+        public Info(string header, Func<IReadOnlyList<Proj>, IEnumerable<string>> @where)
+        {
+            this.Header = Ensure.ThrowIfNull(header, nameof(header));
+            this.Where = Ensure.ThrowIfNull(@where, nameof(where));
+        }
+
+        public string Header { get; }
+
+        public Func<IReadOnlyList<Proj>, IEnumerable<string>> Where { get; }
+    }
+}
---
+++ Plugin/VsProjectSetupPlugin/Tools/Infos.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using VsProjectSetupPlugin.Models;
+
+namespace VsProjectSetupPlugin.Tools
+{
+    public static class Infos
+    {
+        public static  readonly IReadOnlyList<Info> InfoSet =
+            new List<Info>()
+            {
+                new Info(".Net Versions", VersionTools.GetAllVersions)
+            };
+    }
+}
